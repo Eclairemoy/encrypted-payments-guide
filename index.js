9fb9fbe126461905ad4f4217127da933f8b6a1b1
Liz Moy <lizmoy@Lizs-MacBook-Pro.local>
2023-02-12 17:53:39 +0000

    adding stripe

--- index.js
+++ index.js
@@ -12,7 +12,20 @@ const PaymentProviders = {
 }
 
 app.post('/api/checkout', async (req, res) => {
-  // code goes here
+  const { card } = req.body
+  console.log(card)
+  
+  const paymentOptions = {
+    card: card,
+    amount: 100,
+    currency: 'eur',
+    paymentReference: 'plant_purchase'
+  }
+
+  let paymentProvider = PaymentProviders.Stripe;
+  const payment = await paymentProvider.makePayment(paymentOptions);
+
+  res.send(payment);
 });
 
 app.listen(port, () => {
